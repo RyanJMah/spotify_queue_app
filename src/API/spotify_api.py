61bb472eaa43f322456680a1451830d288c9c1f5
rob-villarre <rob.villarre@gmail.com>
2021-01-16 14:50:43 -0800

    search added

--- src/API/spotify_api.py
+++ src/API/spotify_api.py
@@ -11,9 +11,33 @@ def add_to_queue(uri, username, client_id, client_secret, redirect_uri):
         sp = spotipy.Spotify(auth=token)
         client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 
+        #make to add to not only actice device
         sp.add_to_queue(uri)
     else:
         print("Can't get token for", username)
+
+def user_info(username, client_id, client_secret, redirect_uri):
+    scope = 'user-follow-modify'
+    token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
+
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+
+        #make to add to not only actice device
+        print(sp.current_user())
+    else:
+        print("Can't get token for", username)
+
+def search(q):
+    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=cid, client_secret=secret))
+    results = sp.search(q='The weekend', type='track', limit = 10, offset = 0, market = 'US')
+
+    tracks = []
+    for i in results['tracks']['items']:
+        tracks.append(i['id'])
+
+    return tracks
 
 
 cid = 'e928528d94e84800aa53f32758c82ccc'
@@ -31,7 +55,9 @@ if __name__ == "__main__":
         print("Usage: %s username" % (sys.argv[0],))
         sys.exit()
 
-    add_to_queue('6rPO02ozF3bM7NnOV4h6s2', username, cid, secret, "http://localhost/")
+    #add_to_queue('0VjIjW4GlUZAMYd2vXMi3b', username, cid, secret, "http://localhost/")
+    #user_info(username, cid, secret, "http://localhost/")
+    #print(search('despacito'))
 
 
 #token = util.prompt_for_user_token(username, scope[1], client_id = cid, client_secret = secret, redirect_uri='http://localhost/')
