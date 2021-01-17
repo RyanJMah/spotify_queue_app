07ccd6b180e5b74d620e7f6ddf57004e34d5543d
normanxzhang <norman.x.zhang@gmail.com>
2021-01-16 18:36:56 -0700

    Creating dashboardSQL file

--- src/web/dashboard_routes.py
+++ src/web/dashboard_routes.py
@@ -3,14 +3,35 @@ import flask
 import flask_login
 # import handlers
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_mysqldb import MySQL
 
 app_dashboard = flask.Blueprint("app_dashboard", __name__, template_folder = "templates")
 
+app_dashboard.config['MYSQL_USER'] = ''
+app_dashboard.config['MYSQL_PASSWORD'] = ''
+app_dashboard.config['MYSQL_HOST'] = ''
+app_dashboard.config['MYSQL_DB'] = ''
+app_dashboard.config['MYSQL_CURSORCLASS'] = ''
+mysql = MySQL(app_dashboard)
 
-@app_dashboard.route("/dashboard")
-@flask_login.login_required
+@app_dashboard.route("/dashboard", methods = ["GET", "POST", "DELETE"])
 def dashboard():
+    if flask.request.method == "POST":
+        print(flask.request.json)
+    
+    elif flask.request.method == "DELETE":  # Deleting a song from the queue located in the nono table in the database
+        cur = mysql.connection.cursor()
+        cur.execute('''DELETE FROM  ''')
+
+    else:  #GET
+        queue = flask.request.args.get('????')  # updated queue in the dashboard from the webserver
+        return render_template("dashboard.html", queue=queue )
+
     return flask.render_template("dashboard.html")
+
+
+
+
 
 """
 @app_routes.route("/dashboard", methods = ["GET", "POST"])
