0cced7a1dc2dd7dc1723bd038c4f2575e698a5e4
rob-villarre <rob.villarre@gmail.com>
2021-01-16 18:26:05 -0800

    SQL stuff added

--- src/SQL/sql.py
+++ src/SQL/sql.py
@@ -1,13 +1,33 @@
 import mysql.connector
+from credentials import *
 import pandas as pd
 
 
-if __name__ == "__main__":
-
+def connect():
     db = mysql.connector.connect(
-            host = "",
-            user = "admin",
-            password = "",
-            database = ""
+            host = MARIADB_HOST,
+            user = MARIADB_USER,
+            password = MARIADB_PASSWORD,
+            database = MARIADB_DATABASE
         )
+    return db
+
+def read_queue(db = connect()):
+    df = pd.read_sql("SELECT * FROM queues", db)
     db.close()
+    return df
+
+def add_queue(guest_user, host_user, song_name, artist_name, time, db = connect()):
+    cursor = db.cursor()
+    sql = "INSERT INTO queues(guest_user, host_user, song_name, artist_name, time) VALUES (%s, %s, %s, %s, %s)"
+    val = (guest_user, host_user, song_name, artist_name, time)
+    cursor.execute(sql,val)
+    db.commit()
+
+    db.close()
+
+def del_queue(db = connect()):
+
+if __name__ == "__main__":
+    add_queue('test','test','test','test','0000-00-00')
+    print(read_queue())
