7014c710099993497b20b041ac89f47ba6e81d36
rob-villarre <rob.villarre@gmail.com>
2021-01-16 19:12:15 -0800

    API done iish

--- src/API/spotify_api.py
+++ src/API/spotify_api.py
@@ -4,6 +4,7 @@ import sys
 import spotipy.util as util
 import random
 import string
+
 username = 'admin'
 token = 'BQD2R3QxQCSe7RMyFkvV0VGlVQ4eDdzBrwYVOKqEF8GInZp1kSkgLKQ9We5B1XG5lkGa5rv11Jsj5oMGsmhLPXfF4aeEhq-aLG_KGuoKP7Etv6E9iJatERHimMr-3aFxwBPlpstWt2vizkhR1knh06wP6kqlJRf643WHntRXM_sx9GmpuQ'
 sp = spotipy.Spotify(auth=token)
@@ -11,7 +12,7 @@ cid = "41e6ecfe5cd341e2b51fd54476a626e6"
 secret = "1fca15b9415043db87a3571d1ecc90f9"
 
 def search(q):
-    results = sp.search(q, type='track', limit = 10, offset = 0, market = 'US')
+    results = sp.search(q, type='track', limit = 5, offset = 0, market = 'US')
 
     tracks = []
     for i in results['tracks']['items']:
@@ -19,10 +20,15 @@ def search(q):
 
     return tracks
 
+def add_to_queue(song_id):
+    sp.add_to_queue(song_id)
+    
+def skip_track():
+    sp.next_track()
 
 if __name__ == "__main__":
-    #print(sp.devices())
+    pass
     #sp.add_to_queue('4l62h4tiuUwn7eD6hxMlVQ', device_id='780ae0669fd0cf51fe0cc44eb63c7b0694fc6340')
     #print(search('its every day bro'))
-    #sp.next_track(device_id='780ae0669fd0cf51fe0cc44eb63c7b0694fc6340')
+    #add_to_queue('4l62h4tiuUwn7eD6hxMlVQ')
     #print(sp.current_user())
